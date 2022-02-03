369c93cfb2763d6341208c9ee38fdd7270be23c0
AlanCristian <alancristian181@gmail.com>
2022-02-03 15:19:21 -0300

    inclusao de route table association

--- helloworld.tf
+++ helloworld.tf
@@ -45,15 +45,46 @@ resource "aws_route_table" "rotas_brq" {
   }
 }
 
-/*resource "aws_subnet" "subrede_brq" {
+resource "aws_subnet" "subrede_brq" {
    vpc_id     = aws_vpc.vpc_brq.id
    cidr_block = "10.0.1.0/24"
-
+   availability_zone = "us-east-1a"
    tags = {
      Name = "RonyRustico"
    }
- }*/
+ }
 
+resource "aws_route_table_association" "associacao" {
+  subnet_id      = aws_subnet.subrede_brq.id
+  route_table_id = aws_route_table.rotas_brq.id
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
 
 /*resource "aws_instance" "ola-mundo" {
  ami           = "ami-04505e74c0741db8d"
