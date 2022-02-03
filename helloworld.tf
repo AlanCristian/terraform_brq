a141fe4b11d0dfbe990abf4671a6500d02543952
AlanCristian <alancristian181@gmail.com>
2022-02-03 16:58:13 -0300

    inclusão do código inteiro

--- helloworld.tf
+++ helloworld.tf
@@ -1,22 +1,21 @@
 terraform {
- required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "3.74.0"
-   }
- }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.0"
+    }
+  }
 }
 
 provider "aws" {
- region     = "us-east-1"
- #access_key = ""
- #secret_key = ""
+  region     = "us-east-1"
+
 }
 
 resource "aws_vpc" "vpc_brq" {
   cidr_block = "10.0.0.0/16"
   tags = {
-      Name = "VPC_legal"
+    Name = "VPC_legal"
   }
 }
 
@@ -36,8 +35,8 @@ resource "aws_route_table" "rotas_brq" {
   }
 
   route {
-    ipv6_cidr_block        = "::/0"
-    gateway_id = aws_internet_gateway.gw_brq.id
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw_brq.id
   }
 
   tags = {
@@ -46,13 +45,13 @@ resource "aws_route_table" "rotas_brq" {
 }
 
 resource "aws_subnet" "subrede_brq" {
-   vpc_id     = aws_vpc.vpc_brq.id
-   cidr_block = "10.0.1.0/24"
-   availability_zone = "us-east-1a"
-   tags = {
-     Name = "RonyRustico"
-   }
- }
+  vpc_id            = aws_vpc.vpc_brq.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "RonyRustico"
+  }
+}
 
 resource "aws_route_table_association" "associacao" {
   subnet_id      = aws_subnet.subrede_brq.id
@@ -60,30 +59,32 @@ resource "aws_route_table_association" "associacao" {
 }
 
 resource "aws_security_group" "firewall" {
-  name        = "abrir portas"
-  description = "abrir portas 22 (SSH), 443 (HTTPS e 80 (HTTP))"
+  name        = "abrir_portas"
+  description = "Abrir porta 22 (SSH), 443 (HTTPS) e 80 (HTTP)"
   vpc_id      = aws_vpc.vpc_brq.id
 
   ingress {
-    description      = "HTTPS"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -103,41 +104,36 @@ resource "aws_network_interface" "interface_rede" {
   subnet_id       = aws_subnet.subrede_brq.id
   private_ips     = ["10.0.1.51"]
   security_groups = [aws_security_group.firewall.id]
-
   tags = {
     Name = "Weverton"
   }
 }
 
 resource "aws_eip" "ip_publico" {
- vpc                       = true
- network_interface         = aws_network_interface.interface_rede.id
- associate_with_private_ip = "10.0.1.51"
- depends_on                = [aws_internet_gateway.gw_brq]
+  vpc                       = true
+  network_interface         = aws_network_interface.interface_rede.id
+  associate_with_private_ip = "10.0.1.51"
+  depends_on                = [aws_internet_gateway.gw_brq]
 }
 
-/*resource "aws_instance" "hello-world" {
- ami               = "ami-04505e74c0741db8d"
- instance_type     = "t2.micro"
- availability_zone = "us-east-1a"
+
+resource "aws_instance" "app_web" {
+  ami               = "ami-04505e74c0741db8d"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
   network_interface {
-   device_index         = 0
-   network_interface_id = aws_network_interface.interface.id
- }
- user_data = <<-EOF
+    device_index         = 0
+    network_interface_id = aws_network_interface.interface_rede.id
+  }
+  user_data = <<-EOF
                #! /bin/bash
                sudo apt-get update -y
                sudo apt-get install -y apache2
                sudo systemctl start apache2
                sudo systemctl enable apache2
-               sudo bash -c 'echo "<h1>ESTOU RODANDO</h1>"  > /var/www/html/index.html'
+               sudo bash -c 'echo "<h1>Estou On - Aula de Terraform - Alan</h1>"  > /var/www/html/index.html'
              EOF
-}*/
-
-/*resource "aws_instance" "ola-mundo" {
- ami           = "ami-04505e74c0741db8d"
- instance_type = "t2.micro"
- tags = {
-      Name = "Robson"
- }
-}*/
+  tags = {
+    Name = "RaphaelVeiga"
+  }
+}
